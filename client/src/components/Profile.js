3a50f27edcb5d7342df18aa73425383feca445f7
James Yeates <jimibue@gmail.com>
2020-06-30 10:26:31 -0600

    profile p1

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,13 +2,34 @@ import React from "react";
 import { AuthConsumer } from "../providers/AuthProvider";
 import { Container, Divider } from "semantic-ui-react";
 
-export default class Profile extends React.Component {
+class Profile extends React.Component {
+  state = { editing: false, formValues: { name: "", email: "" } };
+
+  componentDidMount() {
+    const {
+      auth: {
+        user: { name, email },
+      },
+    } = this.props;
+    this.setState({ formValues: { name, email } });
+  }
   render() {
+    const {
+      formValues: { name, email },
+    } = this.state;
     return (
       <Container>
         <Divider />
         <h1>Profile</h1>
+        <h1>name: {name}</h1>
+        <h1>email: {email}</h1>
       </Container>
     );
   }
 }
+
+const ConnectedProfile = (props) => (
+  <AuthConsumer>{(auth) => <Profile {...props} auth={auth} />}</AuthConsumer>
+);
+
+export default ConnectedProfile;
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -7,14 +7,14 @@ class Register extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { email, password, passwordConfirmation } = this.state;
+    const { email, password, passwordConfirmation, name } = this.state;
     const {
       auth: { handleRegister },
       history,
     } = this.props;
 
     if (password === passwordConfirmation)
-      handleRegister({ email, password, passwordConfirmation }, history);
+      handleRegister({ email, password, passwordConfirmation, name }, history);
     else alert("Passwords Do Not Match!");
   };
 
